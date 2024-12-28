7f303eb9f2a25650555e9c9c503d2cdb5ff1ff56
Ancuta C <cirlan.ancutaa@gmail.com>
2024-12-28 23:55:13 +0200

    change backend url

--- src/api.js
+++ src/api.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8080/api';
+//const API_URL = 'http://localhost:8080/api';
+const API_URL = 'https://deliveryappserver-production.up.railway.app';
+
+
 
 const getAuthHeaders = () => {
   const user = localStorage.getItem('user');
