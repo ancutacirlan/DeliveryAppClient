5f3f3c95a30236ca388b5c2ae68da25ccf9e7d27
Ancuta C <cirlan.ancutaa@gmail.com>
2025-01-12 23:58:08 +0200

    change server api url

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8080/api';
-//const API_URL = 'https://deliveryappserver-production.up.railway.app/api';
+//const API_URL = 'http://localhost:8080/api';
+const API_URL = 'https://deliveryappserver-production.up.railway.app/api';
 
 
 
